ef0819d4c0514c08e6f1cb82f0b89a68041b43b4
gcamposl <gcamposlopes@gmail.com>
2022-12-03 21:34:34 -0300

    mapping to the database with dbcontext

--- Ck.Server.Infra.Data/Context/ApplicationDbContext.cs
+++ Ck.Server.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Threading.Tasks;
+using Ck.Server.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ck.Server.Infra.Data.Context
 {
   public class ApplicationDbContext : DbContext // AQUI NAO ESTA REFERENCIANDO
   {
-    public ApplicationDbContext()
-    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
 
+    public DbSet<Person> People { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
   }
 }
---
+++ Ck.Server.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ck.Server.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ck.Server.Infra.Data.Maps
+{
+  public class PersonMap : IEntityTypeConfiguration<Person>
+  {
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+      builder.ToTable("Pessoa");
+      builder.HasKey(c => c.Id);
+      builder.Property(c => c.Id)
+        .HasColumnName("id_pessoa")
+        .UseIdentityColumn();
+
+      builder.Property(c => c.Name)
+        .HasColumnName("nome");
+
+      builder.Property(c => c.Document)
+          .HasColumnName("documento");
+
+      builder.Property(c => c.Phone)
+        .HasColumnName("telefone");
+
+      builder.HasMany(c => c.Plan)
+        .WithOne(p => p.Person)
+        .HasForeignKey(c => c.PersonId);
+    }
+  }
+}
