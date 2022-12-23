a2bccd7b56eba35e032af1fbe9af08f510bb3f54
gcamposl <gcamposlopes@gmail.com>
2022-12-23 20:02:16 -0300

    fix

--- Ck.Server.Application/Services/Interfaces/IPersonService.cs
+++ Ck.Server.Application/Services/Interfaces/IPersonService.cs
@@ -6,8 +6,11 @@ using Ck.Server.Application.DTOs;
 
 namespace Ck.Server.Application.Services.Interfaces
 {
-  public interface IPersonService
-  {
-    Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO);
-  }
+    public interface IPersonService
+    {
+        Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO);
+        Task<ResultService<ICollection<PersonDTO>>> GetAsync();
+        Task<ResultService<PersonDTO>> GetByIdAsync(int id);
+
+    }
 }
--- Ck.Server.Application/Services/PersonService.cs
+++ Ck.Server.Application/Services/PersonService.cs
@@ -10,27 +10,41 @@ using FluentValidation.Validators;
 
 namespace Ck.Server.Application.Services
 {
-  public class PersonService : IPersonService
-  {
-    private readonly IPersonRepository _personRepository;
-    private readonly IMapper _mapper;
-    public PersonService(IPersonRepository personRepository, IMapper mapper)
+    public class PersonService : IPersonService
     {
-      _personRepository = personRepository;
-      _mapper = mapper;
-    }
-    public async Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO)
-    {
-      if (personDTO == null)
-        return ResultService.Fail<PersonDTO>("Objeto deve ser informado!");
+        private readonly IPersonRepository _personRepository;
+        private readonly IMapper _mapper;
+        public PersonService(IPersonRepository personRepository, IMapper mapper)
+        {
+            _personRepository = personRepository;
+            _mapper = mapper;
+        }
+        public async Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO)
+        {
+            if (personDTO == null)
+                return ResultService.Fail<PersonDTO>("Objeto deve ser informado!");
 
-      var result = new PersonDTOValidator().Validate(personDTO);
-      if (!result.IsValid)
-        return ResultService.RequestError<PersonDTO>("Problema de validade!", result);
+            var result = new PersonDTOValidator().Validate(personDTO);
+            if (!result.IsValid)
+                return ResultService.RequestError<PersonDTO>("Problema de validade!", result);
 
-      var person = _mapper.Map<Person>(personDTO);
-      var data = await _personRepository.CreateAsync(person);
-      return ResultService.Ok<PersonDTO>(_mapper.Map<PersonDTO>(data));
+            var person = _mapper.Map<Person>(personDTO);
+            var data = await _personRepository.CreateAsync(person);
+            return ResultService.Ok<PersonDTO>(_mapper.Map<PersonDTO>(data));
+        }
+
+        public async Task<ResultService<ICollection<PersonDTO>>> GetAsync()
+        {
+            var person = await _personRepository.GetPersonAsync();
+            return ResultService.Ok<ICollection<PersonDTO>>(_mapper.Map<ICollection<PersonDTO>>(person));
+        }
+
+        public async Task<ResultService<PersonDTO>> GetByIdAsync(int id)
+        {
+            var person = await _personRepository.GetByIdAsync(id);
+            if (person == null)
+                return ResultService.Fail<PersonDTO>("Pessoa não encontrada");
+            return ResultService.Ok(_mapper.Map<PersonDTO>(person));
+        }
     }
-  }
 }
