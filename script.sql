bc4bb76ec52eee3f0a0f30461a7ce56ea5f165fc
gcamposl <gcamposlopes@gmail.com>
2023-02-08 21:07:55 -0300

    feat: updated sql script

--- script.sql
+++ script.sql
@@ -22,3 +22,10 @@ create table if not exists plano (
 	constraint fk_pessoa_plano foreign key(id_pessoa) references pessoa(id_pessoa),
 	constraint fk_pet_plano foreign key(id_pet) references pet(id_pet)	
 );
+
+create table if usuario
+(
+	id_usuario serial,
+	email varchar,
+	senha varchar
+);
