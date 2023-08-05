a4e4a0d7fe8562e58ee6fc774119cd94dc47242a
gcamposl <gcamposlopes@gmail.com>
2023-08-04 22:38:04 -0300

    format

--- Application/Services/ResultService.cs
+++ Application/Services/ResultService.cs
@@ -32,11 +32,15 @@ namespace Application.Services
             };
         }
 
-        public static ResultService Fail(string message) => new ResultService { IsSuccess = false, Message = message };
-        public static ResultService<T> Fail<T>(string message) => new ResultService<T> { IsSuccess = false, Message = message };
+        public static ResultService Fail(string message)
+            => new ResultService { IsSuccess = false, Message = message };
+        public static ResultService<T> Fail<T>(string message)
+            => new ResultService<T> { IsSuccess = false, Message = message };
 
-        public static ResultService Ok(string message) => new ResultService { Message = message, IsSuccess = true };
-        public static ResultService<T> Ok<T>(T data) => new ResultService<T> { Data = data, IsSuccess = true };
+        public static ResultService Ok(string message)
+            => new ResultService { Message = message, IsSuccess = true };
+        public static ResultService<T> Ok<T>(T data)
+            => new ResultService<T> { Data = data, IsSuccess = true };
     }
 
     public class ResultService<T> : ResultService
