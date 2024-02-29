79e78a4ef7363c5494f4a3780d97c7a6176001a7
gcamposl <gcamposlopes@gmail.com>
2024-02-28 23:38:39 -0300

    feat: configure path for save image

--- Data/Integrations/SavePersonImage.cs
+++ Data/Integrations/SavePersonImage.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain.Integrations;
 
 namespace Data.Integrations
 {
     public class SavePersonImage : ISavePersonImage
     {
-
+        private readonly string Path = "/Users/campos/www/chicktosa-api/Images";
+        public string Save(string base64)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
