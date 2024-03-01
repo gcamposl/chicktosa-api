2a0427cb605ea0b285e1acae323a01093371356f
gcamposl <gcamposlopes@gmail.com>
2024-02-29 23:35:09 -0300

    feat: save base64 image in database

--- Data/Integrations/SavePersonImage.cs
+++ Data/Integrations/SavePersonImage.cs
@@ -4,10 +4,30 @@ namespace Data.Integrations
 {
     public class SavePersonImage : ISavePersonImage
     {
-        private readonly string Path = "/Users/campos/www/chicktosa-api/Images";
-        public string Save(string base64)
+        private readonly string _path;
+
+        public SavePersonImage()
         {
-            throw new NotImplementedException();
+            _path = "/Users/campos/www/chicktosa-api/Images";
+        }
+
+        public string Save(string imgBase64)
+        {
+            var fileExt = imgBase64.Substring(imgBase64.IndexOf("/") + 1,
+                imgBase64.IndexOf("/") - imgBase64.IndexOf("/") - 1); // remove png ou jpeg
+
+            var base64Code = imgBase64.Substring(imgBase64.IndexOf(",") + 1);
+            var imgBytes = Convert.FromBase64String(base64Code);
+
+            var fileName = Guid.NewGuid().ToString() + "." + fileExt;
+
+            using (var imgFile = new FileStream(_path + "/" + fileName, FileMode.Create))
+            {
+                imgFile.Write(imgBytes, 0, imgBytes.Length);
+                imgFile.Flush();
+            }
+
+            return _path + "/" + fileName;
         }
     }
 }
