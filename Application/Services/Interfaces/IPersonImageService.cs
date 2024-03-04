302df1f4d935b1eda8afa42c34cc79cbd868b649
gcamposl <gcamposlopes@gmail.com>
2024-03-04 09:19:47 -0300

    feat: save image in database

--- Application/Services/Interfaces/IPersonImageService.cs
+++ Application/Services/Interfaces/IPersonImageService.cs
@@ -5,5 +5,6 @@ namespace Application.Services.Interfaces
     public interface IPersonImageService
     {
         Task<ResultService> CreateImageBase64Async(PersonImageDTO personImageDTO);
+        Task<ResultService> CreateImageAsync(PersonImageDTO personImageDTO);
     }
 }
--- Application/Services/PersonImageService.cs
+++ Application/Services/PersonImageService.cs
@@ -2,6 +2,7 @@ using Application.DTOs.PersonImage;
 using Application.DTOs.PersonImage.Validations;
 using Application.Services.Interfaces;
 using Domain.Entities;
+using Domain.Integrations;
 using Domain.Repositories;
 
 namespace Application.Services
@@ -10,11 +11,36 @@ namespace Application.Services
     {
         private readonly IPersonRepository _personRepository;
         private readonly IPersonImageRepository _personImageRepository;
+        private readonly ISavePersonImage _savePersonImage;
 
-        public PersonImageService(IPersonRepository personRepository, IPersonImageRepository personImageRepository)
+        public PersonImageService(
+            IPersonRepository personRepository,
+            IPersonImageRepository personImageRepository,
+            ISavePersonImage savePersonImage)
         {
             _personRepository = personRepository;
             _personImageRepository = personImageRepository;
+            _savePersonImage = savePersonImage;
+        }
+
+        public async Task<ResultService> CreateImageAsync(PersonImageDTO personImageDTO)
+        {
+            if (personImageDTO is null)
+                return ResultService.Fail("Objeto deve ser informado.");
+
+            var validations = new PersonImageDTOValidation().Validate(personImageDTO);
+            if (!validations.IsValid)
+                return ResultService.RequestError("Erro na validação do objeto.", validations);
+
+            var person = await _personRepository.GetByIdAsync(personImageDTO.PersonId);
+            if (person is null)
+                return ResultService.Fail($"Pessoa {personImageDTO.PersonId} não foi encontrada");
+
+            var pathImage = _savePersonImage.Save(personImageDTO.Image);
+            var personImage = new PersonImage(person.Id, pathImage, null);
+            await _personImageRepository.CreateAsync(personImage);
+
+            return ResultService.Ok("Imagem salva!");
         }
 
         public async Task<ResultService> CreateImageBase64Async(PersonImageDTO personImageDTO)
