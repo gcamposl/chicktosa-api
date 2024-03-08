3edf82a1b74fb665f9e95359104e534befd91103
gcamposl <gcamposlopes@gmail.com>
2024-03-07 22:39:20 -0300

    fix: save person image fixed

--- Data/Integrations/SavePersonImage.cs
+++ Data/Integrations/SavePersonImage.cs
@@ -14,13 +14,10 @@ namespace Data.Integrations
         public string Save(string imgBase64)
         {
             var fileExt = imgBase64.Substring(imgBase64.IndexOf("/") + 1,
-                imgBase64.IndexOf("/") - imgBase64.IndexOf("/") - 1); // remove png ou jpeg
-
+                imgBase64.IndexOf(";") - imgBase64.IndexOf("/") - 1); // remove png ou jpeg
             var base64Code = imgBase64.Substring(imgBase64.IndexOf(",") + 1);
             var imgBytes = Convert.FromBase64String(base64Code);
-
             var fileName = Guid.NewGuid().ToString() + "." + fileExt;
-
             using (var imgFile = new FileStream(_path + "/" + fileName, FileMode.Create))
             {
                 imgFile.Write(imgBytes, 0, imgBytes.Length);
